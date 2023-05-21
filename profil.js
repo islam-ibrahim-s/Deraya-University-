58350e60202dc355da3dc2868f83676dd5f5c7f0
islam-ibrahim-s <134235453+islam-ibrahim-s@users.noreply.github.com>
2023-05-22 00:38:29 +0300

    Add files via upload

---
+++ profil.js
@@ -0,0 +1,64 @@
+var x = document.getElementById('ul1')
+var y = document.getElementById('Colleges')
+
+y.onmouseover=function(){
+    x.style.display='block'
+}
+y.onmouseout=function(){
+    x.style.display='none'
+}
+
+var p = document.getElementById('BusinessPuap')
+var o = document.getElementById('imgSec2-1')
+
+o.onclick=function(){
+    p.style.display='block'
+}
+o.onmouseout=function(){
+    p.style.display='none'
+}
+
+
+var i = document.getElementById('DentalPuap')
+var z = document.getElementById('imgSec2-2')
+
+z.onclick=function(){
+    i.style.display='block'
+}
+z.onmouseout=function(){
+    i.style.display='none'
+}
+
+var f = document.getElementById('PhysiotherapyPuap')
+var s = document.getElementById('imgSec2-3')
+
+s.onclick=function(){
+    f.style.display='block'
+}
+s.onmouseout=function(){
+    f.style.display='none'
+}
+
+var q = document.getElementById('PharmacyPuap')
+var w = document.getElementById('imgSec2-4')
+
+w.onclick=function(){
+    q.style.display='block'
+}
+w.onmouseout=function(){
+    q.style.display='none'
+}
+
+
+
+
+var c = document.getElementById('video')
+var v = document.getElementById('imgSec1')
+
+v.onclick=function(){
+    c.style.display='block'
+}
+v.onmouseout=function(){
+    c.style.display='none'
+}
+
